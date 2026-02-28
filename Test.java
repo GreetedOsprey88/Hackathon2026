757739e528748950be6b02a1f03b0020c69cea0b
Ryan Alder <rryan.alder88@gmail.com>
2026-02-27 23:09:42 -0800

    Test commit I suppose

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, Ryan Christopher Alder, Junior year at Mount Douglas Secondary School, Victoria, BC!");
+        System.out.println("This is a test.");
+        System.out.println("This is only a test.");
     } // main
 } // class Test
